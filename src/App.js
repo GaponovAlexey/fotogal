ac273652758c5e2ba3ee91c083b0d697ed28e871
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-12-04 21:05:35 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router'
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
+import { Main } from './components/Main'
+
 function App() {
-  return <div>fotogal</div>
+  return (
+    <div>
+      <Header />
+        <Routes>
+          <Route path='/' element={<Main/>} />
+        </Routes>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import { Card } from "./Card"
+
+const srs = 'https://random.imagecdn.app/300/150'
+
+export const Main = () => {
+  return (
+    <div className='main' style={{ backgroundColor: 'green', gap: 22 }}>
+      <div className='main-conteiner' >
+      <Card />
+      </div>
+    </div>
+  )
+}
