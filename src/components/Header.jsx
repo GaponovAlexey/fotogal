578b8d5004c1291aff61b08881b1cdec56a6eca7
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-12-04 22:19:55 +0300

    save

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,8 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <div className='grid' style={{ backgroundColor: 'blue' }}>
-      <img src='https://source.unsplash.com/300x300?mountain' alt='yes' />
-      <img src='https://source.unsplash.com/300x300?nature' alt='yes' />
-      <img src='https://source.unsplash.com/300x300?valley' alt='yes' />
-      <img src='https://source.unsplash.com/300x300?beach' alt='yes' />
-      <img src='https://source.unsplash.com/300x300?ocean' alt='yes' />
-      <img src='https://source.unsplash.com/300x300?water' alt='yes' />
-      <img src='https://source.unsplash.com/300x300?trees' alt='yes' />
-      <img src='https://source.unsplash.com/300x300?lake' alt='yes' />
-      <img src='https://source.unsplash.com/300x300?cliff' alt='yes' />
+    <div  style={{ backgroundColor: 'blue' }}>
+   
     </div>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,21 @@
 import { CardList } from "./pages/CardList"
 
-const srs = 'https://random.imagecdn.app/300/150'
+const srs = 'https://random.imagecdn.app/400/150'
 
 export const Main = () => {
   
   return (
     <div className='main' style={{ backgroundColor: 'green', gap: 22 }}>
-      <div className='main-conteiner' >
-      <CardList srs={srs }/>
+      <div className='grid' >
+      <img src='https://source.unsplash.com/400x400?mountain' alt='yes' />
+      <img src='https://source.unsplash.com/400x400?nature' alt='yes' />
+      <img src='https://source.unsplash.com/400x400?valley' alt='yes' />
+      <img src='https://source.unsplash.com/400x400?beach' alt='yes' />
+      <img src='https://source.unsplash.com/400x400?ocean' alt='yes' />
+      <img src='https://source.unsplash.com/400x400?water' alt='yes' />
+      <img src='https://source.unsplash.com/400x400?trees' alt='yes' />
+      <img src='https://source.unsplash.com/400x400?lake' alt='yes' />
+      <img src='https://source.unsplash.com/400x400?cliff' alt='yes' />
       </div>
     </div>
   )
